804d8f7c94bf8fb4f043974c38bf6fb9c9db60e4
FelipeGiusti-2000 <61150874+FelipeGiusti-2000@users.noreply.github.com>
2020-06-08 12:47:25 -0300

    criacao BigBrothers
    criacao de classe

---
+++ Atividade2/src/br/maua/models/BigBrothers.java
@@ -0,0 +1,10 @@
+package br.maua.models;
+
+import br.maua.enums.TipoDeMembros;
+
+public class BigBrothers extends Membro{
+    
+    public BigBrothers(String nome, String email, TipoDeMembros funcao) {
+        super(nome, email, funcao);
+    }
+}
