dce6e35a4d4d594561677d261ba5aba91f8d7dbf
FelipeGiusti-2000 <61150874+FelipeGiusti-2000@users.noreply.github.com>
2020-06-08 12:46:27 -0300

    criacao ScriptGuys
    classe ScriptGuys

---
+++ Atividade2/src/br/maua/models/ScriptGuys.java
@@ -0,0 +1,9 @@
+package br.maua.models;
+
+import br.maua.enums.TipoDeMembros;
+
+public class ScriptGuys extends Membro{
+    public ScriptGuys(String nome, String email, TipoDeMembros funcao) {
+        super(nome, email, funcao);
+    }
+}
